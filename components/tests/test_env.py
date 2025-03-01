06e555659be1f96ed4f9c8f1e6d983dbc3d0afff
bradyhhhuang <bradyhuang053@gmail.com>
2025-02-28 16:01:16 -0800

    add testing repo secrets

--- components/tests/test_env.py
+++ components/tests/test_env.py
@@ -1,6 +1,7 @@
 '''Implementating a environment test.
 '''
 import sys
+import os
 
 import unittest
 import numpy as np
@@ -25,6 +26,34 @@ class TestEnvironment(unittest.TestCase):
         ''' Test the python version.
         '''
         self.assertGreater(sys.version_info, (3, 12))
+
+    def test_env_vars_google_api_key(self):
+        ''' Test the google_api_key.
+        '''
+        GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+        length_of_the_key = len(GOOGLE_API_KEY)
+        self.assertGreater(length_of_the_key, 0)
+
+    def test_env_vars_langchain_api_key(self):
+        ''' Test the langchain_api_key.
+        '''
+        LANGCHAIN_API_KEY = os.environ.get("LANGCHAIN_API_KEY")
+        length_of_the_key = len(LANGCHAIN_API_KEY)
+        self.assertGreater(length_of_the_key, 0)
+    
+    def test_env_vars_langchain_tracing(self):
+        ''' Test the langchain_tracing.
+        '''
+        LANGSMITH_TRACING = os.environ.get("LANGSMITH_TRACING")
+        length_of_tracing = len(LANGSMITH_TRACING)
+        self.assertGreater(length_of_tracing, 0)
+    
+    def test_env_vars_google_api_key(self):
+        ''' Test the google_api_key.
+        '''
+        LANGSMITH_ENDPOINT = os.environ.get("LANGSMITH_ENDPOINT")
+        length_of_endpoint = len(LANGSMITH_ENDPOINT)
+        self.assertGreater(length_of_endpoint, 0)
 
 if __name__ == '__main__':
     unittest.main()
