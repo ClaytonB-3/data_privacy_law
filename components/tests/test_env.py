85f6750526ee2bdcb16d491c351ad694554cc1d3
bradyhhhuang <bradyhuang053@gmail.com>
2025-03-13 18:38:38 -0700

    test api key

--- components/tests/test_env.py
+++ components/tests/test_env.py
@@ -4,10 +4,10 @@ import sys
 import os
 
 import unittest
+from dotenv import load_dotenv
 
 import numpy as np
 import pandas as pd
-# from dotenv import load_dotenv
 import google.generativeai as genai
 
 
