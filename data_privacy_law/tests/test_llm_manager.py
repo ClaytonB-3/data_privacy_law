03be81fd0a6a5fdd157a8416acc2d83a3ea5f513
ClaytonB-3 <claytonbrock94@gmail.com>
2025-03-15 17:11:12 -0700

    fix parse_bill_info test

--- data_privacy_law/tests/test_llm_manager.py
+++ data_privacy_law/tests/test_llm_manager.py
@@ -1,6 +1,7 @@
 """
 Unittest for the functions in llm_mamager.py
 """
+
 import os
 
 import unittest
@@ -10,40 +11,42 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 import google.generativeai as genai
 
-from llm_manager.experimental_llm_manager import (add_to_faiss_index,
-                                      parse_bill_info,
-                                      extract_text_from_pdf,
-                                      chunk_pdf_pages,
-                                      load_faiss_index,
-                                      calculate_updated_chunk_ids,
-                                      obtain_text_of_chunk,
-                                      llm_simplify_chunk_text,
-                                      get_conversational_chain,
-                                      get_confirmation_result_chain)
-
+from llm_manager.experimental_llm_manager import (
+    add_to_faiss_index,
+    parse_bill_info,
+    extract_text_from_pdf,
+    chunk_pdf_pages,
+    load_faiss_index,
+    calculate_updated_chunk_ids,
+    obtain_text_of_chunk,
+    llm_simplify_chunk_text,
+    get_conversational_chain,
+    get_confirmation_result_chain,
+)
 
 
 class TestPDFExtraction(unittest.TestCase):
     """
-    Testing whether the PDF's extraction function is working properly 
-    and giving response in the right format. 
+    Testing whether the PDF's extraction function is working properly
+    and giving response in the right format.
 
     Args for the function --> pdf_path (str): The path to the PDF file.
     Returns --> list: A list of strings, each representing a page of the PDF file.
     """
+
     def setUp(self):
         """
         Creating a temporary pdf file with 2 pages to test the extract pdf function"
         """
         self.temp_pdf_path = "pdf_created_for_testing.pdf"
-        playground = canvas.Canvas(self.temp_pdf_path, pagesize = letter)
+        playground = canvas.Canvas(self.temp_pdf_path, pagesize=letter)
 
         # Creating the first page
-        playground.drawString(100,750, "This is the first page in the project TPLC")
+        playground.drawString(100, 750, "This is the first page in the project TPLC")
         playground.showPage()
 
         # Creating the second page
-        playground.drawString(100,750, "Second page in the project TPLC")
+        playground.drawString(100, 750, "Second page in the project TPLC")
         playground.save()
 
     def tearDown(self):
@@ -55,7 +58,7 @@ class TestPDFExtraction(unittest.TestCase):
 
     def test_pdf_extraction(self):
         """
-        Passing the created pdf file to the extract_pdf_function 
+        Passing the created pdf file to the extract_pdf_function
         and then comparing the result vs expected
         """
         pages = extract_text_from_pdf(self.temp_pdf_path)
@@ -71,12 +74,15 @@ class TestPDFExtraction(unittest.TestCase):
             self.assertIsInstance(page, str, "Each page should be a string.")
 
         # Verify that expected text appears on each page.
-        self.assertIn("This is the first page in the project TPLC",
-                      pages[0],
-                      "Page 1 text does not match.")
-        self.assertIn("Second page in the project TPLC",
-                      pages[1],
-                      "Page 2 text does not match.")
+        self.assertIn(
+            "This is the first page in the project TPLC",
+            pages[0],
+            "Page 1 text does not match.",
+        )
+        self.assertIn(
+            "Second page in the project TPLC", pages[1], "Page 2 text does not match."
+        )
+
 
 class TestLLMManager(unittest.TestCase):
     """
@@ -84,31 +90,31 @@ class TestLLMManager(unittest.TestCase):
     """
 
     def setUp(self):
-        api_key=os.environ.get("GOOGLE_API_KEY")
+        api_key = os.environ.get("GOOGLE_API_KEY")
         genai.configure(api_key=api_key)
-        self.pdf_path = './pdfs/Texas/HB 186 Social_media_children.pdf'
+        self.pdf_path = "./pdfs/Texas/HB 186 Social_media_children.pdf"
         self.pages_of_pdf = extract_text_from_pdf(self.pdf_path)
         full_pdf_text = "\n".join(self.pages_of_pdf)
         self.bill_info = parse_bill_info(full_pdf_text)
         self.chunk_size = 800
-        self.chunk_texts, self.chunk_metadatas = chunk_pdf_pages(self.pages_of_pdf,
-                                                                 self.pdf_path,
-                                                                 self.chunk_size)
+        self.chunk_texts, self.chunk_metadatas = chunk_pdf_pages(
+            self.pages_of_pdf, self.pdf_path, self.chunk_size
+        )
 
     def test_parse_bill_info(self):
         """
         Test whether parse_bill_info produces expected results.
         """
 
-        self.assertEqual(len(self.bill_info.keys()), 5)
-        self.assertEqual(self.bill_info['State'], 'Texas')
+        self.assertEqual(len(self.bill_info.keys()), 6)
+        self.assertEqual(self.bill_info["State"], "Texas")
 
         # Sometimes it returns different results
         # self.assertEqual(self.bill_info['Title'],
         #                  'Texas: Act prohibiting social media use by children')
-        self.assertEqual(self.bill_info['Date'], '09012025')
-        self.assertEqual(self.bill_info['Type'], 'State level sectoral')
-        self.assertEqual(self.bill_info['Sector'], 'Children’s Data Protection')
+        self.assertEqual(self.bill_info["Date"], "09012025")
+        self.assertEqual(self.bill_info["Type"], "State level sectoral")
+        self.assertEqual(self.bill_info["Sector"], "Children’s Data Protection")
 
     def test_chunk_pdf_pages(self):
         """
@@ -122,15 +128,16 @@ class TestLLMManager(unittest.TestCase):
 
         max_page = 0
         for metadata in self.chunk_metadatas:
-            max_page = max(int(metadata['Page']), max_page)
+            max_page = max(int(metadata["Page"]), max_page)
 
         self.assertEqual(max_page, len(self.pages_of_pdf))
         for metadata in self.chunk_metadatas:
             self.assertEqual(len(metadata), len(["Source", "Page", "Filename", "Path"]))
-            self.assertEqual(metadata['Source'], self.pdf_path)
-            self.assertTrue(1 <= int(metadata['Page']))
-            self.assertEqual(metadata['Filename'],
-                             self.pdf_path.rsplit('/', maxsplit=1)[-1])
+            self.assertEqual(metadata["Source"], self.pdf_path)
+            self.assertTrue(1 <= int(metadata["Page"]))
+            self.assertEqual(
+                metadata["Filename"], self.pdf_path.rsplit("/", maxsplit=1)[-1]
+            )
 
     def test_calculate_updated_chunk_ids(self):
         """
@@ -142,13 +149,13 @@ class TestLLMManager(unittest.TestCase):
             {"Title": "Texas: Title_A", "Page": "2"},
         ]
         expected_1 = [
-             "Texas:_Title_A_Page_1_ChunkNo_0",
-             "Texas:_Title_A_Page_1_ChunkNo_1",
-             "Texas:_Title_A_Page_2_ChunkNo_0",
+            "Texas:_Title_A_Page_1_ChunkNo_0",
+            "Texas:_Title_A_Page_1_ChunkNo_1",
+            "Texas:_Title_A_Page_2_ChunkNo_0",
         ]
         test_case_1_result = calculate_updated_chunk_ids(test_case_1)
         for ind in range(len(test_case_1)):
-            self.assertEqual(expected_1[ind], test_case_1_result[ind]['Chunk_id'])
+            self.assertEqual(expected_1[ind], test_case_1_result[ind]["Chunk_id"])
 
         test_case_2 = [
             {"Page": "1"},
@@ -157,14 +164,14 @@ class TestLLMManager(unittest.TestCase):
             {"Title": "Texas: Title_A"},
         ]
         expected_2 = [
-             "unknown_Page_1_ChunkNo_0",
-             "Texas:_Title_A_Page_1_ChunkNo_0",
-             "Texas:_Title_A_Page_2_ChunkNo_0",
-             "Texas:_Title_A_Page_1_ChunkNo_0"   # A Potential Bug
+            "unknown_Page_1_ChunkNo_0",
+            "Texas:_Title_A_Page_1_ChunkNo_0",
+            "Texas:_Title_A_Page_2_ChunkNo_0",
+            "Texas:_Title_A_Page_1_ChunkNo_0",  # A Potential Bug
         ]
         test_case_2_result = calculate_updated_chunk_ids(test_case_2)
         for ind in range(len(test_case_2)):
-            self.assertEqual(expected_2[ind], test_case_2_result[ind]['Chunk_id'])
+            self.assertEqual(expected_2[ind], test_case_2_result[ind]["Chunk_id"])
 
     @patch("llm_manager.experimental_llm_manager.load_faiss_index")
     def test_obtain_text_of_chunk(self, mock_load_faiss):
@@ -176,27 +183,29 @@ class TestLLMManager(unittest.TestCase):
 
         # test1 - general situation
         mock_doc1 = MagicMock(spec=[])
-        mock_doc1.page_content = 'text123'
+        mock_doc1.page_content = "text123"
 
         mock_doc2 = MagicMock(spec=[])
-        mock_doc2.metadata = {'Chunk_id' : 1}
-        mock_doc2.page_content = 'text123'
+        mock_doc2.metadata = {"Chunk_id": 1}
+        mock_doc2.page_content = "text123"
 
         mock_doc3 = MagicMock(spec=[])
-        mock_doc3.metadata = {'Chunk_id' : 1}
-        mock_doc3.page_content = 'text456'
+        mock_doc3.metadata = {"Chunk_id": 1}
+        mock_doc3.page_content = "text456"
 
-        mock_faiss_instance.docstore._dict = {'Chunk_id_1' : mock_doc1,
-                                              'Chunk_id_2' : mock_doc2,
-                                              'Chunk_id_3' : mock_doc3}
-        self.assertEqual(obtain_text_of_chunk(1), 'text123')
+        mock_faiss_instance.docstore._dict = {
+            "Chunk_id_1": mock_doc1,
+            "Chunk_id_2": mock_doc2,
+            "Chunk_id_3": mock_doc3,
+        }
+        self.assertEqual(obtain_text_of_chunk(1), "text123")
         self.assertEqual(obtain_text_of_chunk(2), None)
 
         # test2 - No page_content
         mock_doc1 = MagicMock(spec=[])
-        mock_doc1.metadata = {'Chunk_id' : 1}
-        mock_faiss_instance.docstore._dict = {'Chunk_id_1' : mock_doc1}
-        self.assertEqual(obtain_text_of_chunk(1), '')
+        mock_doc1.metadata = {"Chunk_id": 1}
+        mock_faiss_instance.docstore._dict = {"Chunk_id_1": mock_doc1}
+        self.assertEqual(obtain_text_of_chunk(1), "")
 
 
 class TestLLMResponse(unittest.TestCase):
@@ -210,7 +219,7 @@ class TestLLMResponse(unittest.TestCase):
     def test_llm_simplify_chunk_text(self, mock_prompt, mock_genai, mock_chain):
         """
         Test llm_simplify_chunk_text runs properly
-        
+
         Args:
             mock_prompt: mock patch for PromptTemplate
             mock_genai: mock genai for ChatGoogleGenerativeAI
@@ -227,7 +236,7 @@ class TestLLMResponse(unittest.TestCase):
     def test_get_confirmation_result_chain(self, mock_prompt, mock_genai, mock_chain):
         """
         Test get_confirmation_result_chai runs properly
-        
+
         Args:
             mock_prompt: mock patch for PromptTemplate
             mock_genai: mock genai for ChatGoogleGenerativeAI
@@ -243,7 +252,7 @@ class TestLLMResponse(unittest.TestCase):
     def test_get_conversational_chain(self, mock_prompt, mock_genai, mock_chain):
         """
         Test get_conversational_chain runs properly
-        
+
         Args:
             mock_prompt: mock patch for PromptTemplate
             mock_genai: mock genai for ChatGoogleGenerativeAI
@@ -314,9 +323,12 @@ completion of the age verification process."""
         )
         self.assertGreater(len(firstresult1), 0)
         self.assertGreater(len(result1), 0)
-        self.assertTrue("""The document database has an answer to your \
+        self.assertTrue(
+            """The document database has an answer to your \
 question. Here is the structured response based \
-on TPLC's database""" in result1)
+on TPLC's database"""
+            in result1
+        )
 
         user_question2 = "Who is the president of USA?"
         chain1 = get_conversational_chain()
@@ -332,8 +344,13 @@ on TPLC's database""" in result1)
                 "answer": firstresult2,
             }
         )
-        self.assertTrue("""Sorry, the LLM cannot currently generate \
-a good enough response""" in result2, msg=result2)
+        self.assertTrue(
+            """Sorry, the LLM cannot currently generate \
+a good enough response"""
+            in result2,
+            msg=result2,
+        )
+
 
 class TestFAISSIndex(unittest.TestCase):
     """
@@ -344,11 +361,9 @@ class TestFAISSIndex(unittest.TestCase):
     @patch("llm_manager.experimental_llm_manager.GoogleGenerativeAIEmbeddings")
     @patch("llm_manager.experimental_llm_manager.os.path.exists")
     @patch("llm_manager.experimental_llm_manager.calculate_updated_chunk_ids")
-    def test_add_to_faiss_index_create_new(self,
-                                           mock_chunks,
-                                           mock_exists,
-                                           mock_embeddings,
-                                           mock_faiss):
+    def test_add_to_faiss_index_create_new(
+        self, mock_chunks, mock_exists, mock_embeddings, mock_faiss
+    ):
         """
         Test whether add_to_faiss_index can create new index properly
 
@@ -369,7 +384,9 @@ class TestFAISSIndex(unittest.TestCase):
         mock_faiss.from_texts.return_value = mock_faiss_instance
 
         # Run the function
-        add_to_faiss_index(chunk_texts=["test chunk"], chunk_metadatas=[{"Chunk_id": "123"}])
+        add_to_faiss_index(
+            chunk_texts=["test chunk"], chunk_metadatas=[{"Chunk_id": "123"}]
+        )
 
         # Check if FAISS was called to create a new index
         mock_faiss.from_texts.assert_called_once()
@@ -378,13 +395,15 @@ class TestFAISSIndex(unittest.TestCase):
     @patch("llm_manager.experimental_llm_manager.FAISS")
     @patch("llm_manager.experimental_llm_manager.GoogleGenerativeAIEmbeddings")
     @patch("llm_manager.experimental_llm_manager.os.path.exists")
-    @patch("llm_manager.experimental_llm_manager.calculate_updated_chunk_ids",)
-    def test_add_to_faiss_index_load_and_add_texts(self, mock_chunks,
-                                                   mock_exists, mock_embeddings,
-                                                   mock_faiss):
+    @patch(
+        "llm_manager.experimental_llm_manager.calculate_updated_chunk_ids",
+    )
+    def test_add_to_faiss_index_load_and_add_texts(
+        self, mock_chunks, mock_exists, mock_embeddings, mock_faiss
+    ):
         """
         Test whether add_to_faiss_index can load existing index and add texts properly
-        
+
         Args:
             mock_chunks: mock patch for calculate_updated_chunk_ids
             mock_exists: mock patch for os.path.exists
@@ -400,7 +419,7 @@ class TestFAISSIndex(unittest.TestCase):
 
         # Mock FAISS load
         mock_faiss_instance = MagicMock()
-        mock_faiss_instance.docstore._dict = {"123":"123"}
+        mock_faiss_instance.docstore._dict = {"123": "123"}
         mock_faiss.load_local.return_value = mock_faiss_instance
 
         add_to_faiss_index(["new chunk"], [{"Chunk_id": "456"}])  # New ID
@@ -408,22 +427,22 @@ class TestFAISSIndex(unittest.TestCase):
 
         # Check if FAISS was called to create a add new index
         mock_faiss_instance.add_texts.assert_called_once_with(
-            texts=["new chunk"],
-            metadatas=[{"Chunk_id": "456"}],
-            ids=["456"]
+            texts=["new chunk"], metadatas=[{"Chunk_id": "456"}], ids=["456"]
         )
         mock_faiss_instance.save_local.assert_called_once()
 
     @patch("llm_manager.experimental_llm_manager.FAISS")
     @patch("llm_manager.experimental_llm_manager.GoogleGenerativeAIEmbeddings")
     @patch("llm_manager.experimental_llm_manager.os.path.exists")
-    @patch("llm_manager.experimental_llm_manager.calculate_updated_chunk_ids", )
-    def test_add_to_faiss_index_load_error(self, mock_chunks,
-                                           mock_exists,
-                                           mock_embeddings, mock_faiss):
+    @patch(
+        "llm_manager.experimental_llm_manager.calculate_updated_chunk_ids",
+    )
+    def test_add_to_faiss_index_load_error(
+        self, mock_chunks, mock_exists, mock_embeddings, mock_faiss
+    ):
         """
         Test whether add_to_faiss_index can handle load errors properly
-        
+
         Args:
             mock_chunks: mock patch for calculate_updated_chunk_ids
             mock_exists: mock patch for os.path.exists
@@ -455,7 +474,7 @@ class TestFAISSIndex(unittest.TestCase):
     def test_load_faiss_index(self, mock_embeddings, mock_faiss):
         """
         Test whether load_faiss_index works properly
-        
+
         Args:
             mock_embeddings: mock patch for GoogleGenerativeAIEmbeddings
             mock_faiss: mock patch for FAISS
@@ -470,5 +489,5 @@ class TestFAISSIndex(unittest.TestCase):
         mock_faiss.load_local.assert_called_once()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
