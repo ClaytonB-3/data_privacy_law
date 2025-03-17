80e96ecf8aae1d4e8a4aee2811832dc77f32fb67
bradyhhhuang <bradyhuang053@gmail.com>
2025-03-17 15:59:08 -0700

    add tests for extract_text_from_pdf

--- data_privacy_law/tests/test_db_manager.py
+++ data_privacy_law/tests/test_db_manager.py
@@ -32,54 +32,62 @@ class TestPDFExtraction(unittest.TestCase):
     and giving response in the right format.
     """
 
-    def setUp(self):
+    # def setUp(self):
+    #     """
+    #     Creating a temporary pdf file with 2 pages to test the extract pdf function"
+    #     """
+    #     self.temp_pdf_path = "pdf_created_for_testing.pdf"
+    #     playground = canvas.Canvas(self.temp_pdf_path, pagesize=letter)
+
+    #     # Creating the first page
+    #     playground.drawString(100, 750, "This is the first page in the project TPLC")
+    #     playground.showPage()
+
+    #     # Creating the second page
+    #     playground.drawString(100, 750, "Second page in the project TPLC")
+    #     playground.save()
+
+    # def tearDown(self):
+    #     """
+    #     Deleting the temporary pdf that was created in the setUp function
+    #     """
+    #     if os.path.exists(self.temp_pdf_path):
+    #         os.remove(self.temp_pdf_path)
+
+    # def test_pdf_extraction(self):
+    #     """
+    #     Passing the created pdf file to the extract_pdf_function
+    #     and then comparing the result vs expected
+    #     """
+    #     pages = extract_text_from_pdf(self.temp_pdf_path)
+
+    #     # Check that the output is a list.
+    #     self.assertIsInstance(pages, list, "The output should be a list.")
+
+    #     # Check that we have two pages.
+    #     self.assertEqual(len(pages), 2, "There should be 2 pages extracted.")
+
+    #     # Check that each page is a string.
+    #     for page in pages:
+    #         self.assertIsInstance(page, str, "Each page should be a string.")
+
+    #     # Verify that expected text appears on each page.
+    #     self.assertIn(
+    #         "This is the first page in the project TPLC",
+    #         pages[0],
+    #         "Page 1 text does not match.",
+    #     )
+    #     self.assertIn(
+    #         "Second page in the project TPLC", pages[1], "Page 2 text does not match."
+    #     )
+
+    def test_pdf_extraction_file_not_exist(self):
         """
-        Creating a temporary pdf file with 2 pages to test the extract pdf function"
+        Test Raising FileNotFoundError works properly. 
         """
-        self.temp_pdf_path = "pdf_created_for_testing.pdf"
-        playground = canvas.Canvas(self.temp_pdf_path, pagesize=letter)
-
-        # Creating the first page
-        playground.drawString(100, 750, "This is the first page in the project TPLC")
-        playground.showPage()
-
-        # Creating the second page
-        playground.drawString(100, 750, "Second page in the project TPLC")
-        playground.save()
-
-    def tearDown(self):
-        """
-        Deleting the temporary pdf that was created in the setUp function
-        """
-        if os.path.exists(self.temp_pdf_path):
-            os.remove(self.temp_pdf_path)
-
-    def test_pdf_extraction(self):
-        """
-        Passing the created pdf file to the extract_pdf_function
-        and then comparing the result vs expected
-        """
-        pages = extract_text_from_pdf(self.temp_pdf_path)
-
-        # Check that the output is a list.
-        self.assertIsInstance(pages, list, "The output should be a list.")
-
-        # Check that we have two pages.
-        self.assertEqual(len(pages), 2, "There should be 2 pages extracted.")
-
-        # Check that each page is a string.
-        for page in pages:
-            self.assertIsInstance(page, str, "Each page should be a string.")
-
-        # Verify that expected text appears on each page.
-        self.assertIn(
-            "This is the first page in the project TPLC",
-            pages[0],
-            "Page 1 text does not match.",
-        )
-        self.assertIn(
-            "Second page in the project TPLC", pages[1], "Page 2 text does not match."
-        )
+        temp_pdf_path ='/abc.pdf'
+        with self.assertRaises(FileNotFoundError):
+            extract_text_from_pdf(temp_pdf_path)
 
 
 class TestDBManager(unittest.TestCase):
